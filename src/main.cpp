57b8c910accad416ce994ff754ca2e081b445910
Kai Ruth <contact@kairuth.ch>
2026-07-06 12:14:25 +0100

    setup Nested sampling and write to files

---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include <armadillo>
+#include <cstddef>
+#include <ffunc.hpp>
+#include <fstream>
+
+#include "flowsheet.hpp"
+#include "nsfeas.hpp"
+
+int main()
+{
+  arma::arma_rng::set_seed(42);
+  mc::FFGraph dag;
+  Flowsheet fs(dag);
+
+  mc::NSFEAS NS;
+  NS.options.FEASCRIT  = mc::NSFEAS::Options::VAR;
+  NS.options.NUMLIVE   = 512;
+  NS.options.NUMPROP   = 16;
+  NS.options.MAXITER   = 20'000;
+  NS.options.MAXTHREAD = 0;
+  NS.options.DISPLEVEL = 1;
+
+  NS.set_dag(dag);
+  NS.set_control(fs.D, {298.15, 2.0, 298.15, 2.0},
+                 {353.15, 30.0, 353.15, 30.0});
+  const auto th = Flowsheet::theta_nominal();
+  NS.set_parameter(fs.P, {th[0], th[1], th[0], th[1]});  // nominal CSP
+  NS.set_constant(fs.C);
+  NS.set_constraint(fs.G);
+
+  if (!NS.setup()) return 1;
+  // run nested sampling
+  int status = NS.sample({0.05, 0.55, 0.70});  // constraint thresholds
+
+  NS.stats.display();
+  // refer to: _liveFEAS.insert in MANGUS NSFEAS
+  auto dump = [](const auto& pts, const std::string& name) {
+    std::ofstream f(name);
+    f << "crit,feasprob,T1,tau1,T2,tau2\n";
+    for (const auto& [crit, pt] : pts)
+    {
+      f << crit << "," << std::get<1>(pt);
+      for (size_t i{}; i < 4; ++i)
+      {
+        f << "," << std::get<0>(pt)[i];
+      };
+      f << "\n";
+    }
+  };
+
+
+  dump(NS.live_points(), "live_points.csv");        // DS sample
+  dump(NS.dead_points(), "dead_points.csv");        // killed during run
+  dump(NS.discard_points(), "discard_points.csv");  // rejected proposals
+
+  return status;
+}
